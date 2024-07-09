b409e23853609628fab156a24d405915e70d60ff
TarasYarovyi <yarovoy.taras@gmail.com>
2024-07-09 07:10:51 +0200

    Optimizing

--- The HackerRank Interview Preparation Kit/Arrays/Hash Tables- Ice Cream Parlor.js
+++ The HackerRank Interview Preparation Kit/Arrays/Hash Tables- Ice Cream Parlor.js
@@ -1,20 +1,33 @@
 // https://www.hackerrank.com/challenges/ctci-ice-cream-parlor/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=search
 
+// function whatFlavors(cost, money) {
+//   // Write your code here
+//   let first, second;
+//   cost.findIndex(
+//     (el1, index1) =>
+//       el1 +
+//       cost.find((el2, index2) => {
+//         if (index1 !== index2 && el1 + el2 === money) {
+//           first = index1 + 1;
+//           second = index2 + 1;
+//           return true;
+//         } else false;
+//       })
+//   );
+//   console.log(first, second);
+// }
+
 function whatFlavors(cost, money) {
-  // Write your code here
-  let first, second;
-  cost.findIndex(
-    (el1, index1) =>
-      el1 +
-      cost.find((el2, index2) => {
-        if (index1 !== index2 && el1 + el2 === money) {
-          first = index1 + 1;
-          second = index2 + 1;
-          return true;
-        } else false;
-      })
-  );
-  console.log(first, second);
+  const hash = {};
+  for (let i = cost.length - 1; i >= 0; i--) {
+    hash[cost[i]] ? hash[cost[i]].push(i + 1) : (hash[cost[i]] = [i + 1]);
+  }
+
+  for (let i = cost.length - 1; i >= 0; i--) {
+    const el = cost[i];
+    if (hash[money - el]) {
+    }
+  }
 }
 
-console.log(whatFlavors([4, 1, 5, 3, 2], 4));
+whatFlavors([4, 1, 4, 3, 2], 4);
