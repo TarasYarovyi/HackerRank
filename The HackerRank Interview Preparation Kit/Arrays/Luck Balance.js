608675f8bff09244a8b65b9e08b9bd06b621e4f0
TarasYarovyi <yarovoy.taras@gmail.com>
2024-07-15 09:50:28 +0200

    Draft

--- The HackerRank Interview Preparation Kit/Arrays/Luck Balance.js
+++ The HackerRank Interview Preparation Kit/Arrays/Luck Balance.js
@@ -2,26 +2,25 @@
 // https://www.hackerrank.com/challenges/luck-balance/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=greedy-algorithms
 
 function luckBalance(k, contests) {
-  let important = 0;
   const ranking = contests.sort((a, b) => {
-    if (a[1] === 1) {
-      important++;
-    }
     if (a[1] === b[1]) {
       return a[0] - b[0];
     }
   });
-  console.log(important);
-  return ranking.slice(k).reduce((acc, curr) => (acc += curr[0]), 0);
+  let toWin = ranking.filter((el) => el[1] > 0).length - k;
+
+  return ranking.reduce(
+    (acc, curr, index) => (index < toWin ? acc - curr[0] : acc + curr[0]),
+    0
+  );
 }
 
 console.log(
-  luckBalance(3, [
+  luckBalance(2, [
     [5, 1],
+    [4, 0],
+    [6, 1],
     [2, 1],
-    [1, 1],
-    [8, 1],
-    [10, 0],
-    [5, 0],
+    [8, 0],
   ])
 );
