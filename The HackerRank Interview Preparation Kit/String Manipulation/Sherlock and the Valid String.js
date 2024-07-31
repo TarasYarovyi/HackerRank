f18483c5ba69e4d17f7e358e942b372086fe1730
TarasYarovyi <yarovoy.taras@gmail.com>
2024-07-31 07:25:40 +0200

    draft

--- The HackerRank Interview Preparation Kit/String Manipulation/Sherlock and the Valid String.js
+++ The HackerRank Interview Preparation Kit/String Manipulation/Sherlock and the Valid String.js
@@ -1,4 +1,14 @@
 // Sherlock and the Valid String
 // https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
 
-function isValid(s) {}
+function isValid(s) {
+  const obj = {};
+  for (let i = 0; i < s.length; i++) {
+    let symbol = s[i];
+    obj[symbol] ? obj[symbol]++ : (obj[symbol] = 1);
+  }
+
+  return obj;
+}
+
+console.log(isValid("abcdefghhgfedecba"));
