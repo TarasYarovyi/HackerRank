123eb7d3b095f5adf07895d73dd035479e766812
TarasYarovyi <yarovoy.taras@gmail.com>
2024-08-07 07:21:22 +0200

    Draft

--- The HackerRank Interview Preparation Kit/String Manipulation/Special String Again.js
+++ The HackerRank Interview Preparation Kit/String Manipulation/Special String Again.js
@@ -1,2 +1,12 @@
 // Special String Again
 // https://www.hackerrank.com/challenges/special-palindrome-again/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+
+function substrCount(s) {
+  let result = [];
+  for (let i = 0; i < s.length; i++) {
+    result.push(s.substring(i).match(/(.+).\1/));
+  }
+  return result;
+}
+
+console.log(substrCount("abcbaba"));
