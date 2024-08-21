25a01f4c50abf57082282fdebf02f564f4442c6b
TarasYarovyi <yarovoy.taras@gmail.com>
2024-08-21 06:56:27 +0200

    Draft

--- The HackerRank Interview Preparation Kit/Arrays/Dictionaries and Hashmaps/Count Triplets.js
+++ The HackerRank Interview Preparation Kit/Arrays/Dictionaries and Hashmaps/Count Triplets.js
@@ -1,2 +1,5 @@
 // Count Triplets
 // https://www.hackerrank.com/challenges/count-triplets-1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+
+function countTriplets(arr, r) {}
+console.log([1, 3, 9, 9, 27, 81], 3);
